b52401edf0eb44efc011a4733838aaff625ba745
emssurya <emssurya56@gmail.com>
2025-12-23 12:39:46 +0530

    Add files via upload

---
+++ arith.php
@@ -0,0 +1,61 @@
+<?php
+//Define two numbers
+$x = 10;
+$y = 3;
+
+
+//Addition
+echo "Addition: " . ($x+$y). "\n";
+
+//Subtraction
+echo "Subtraction: " . ($x-$y). "\n";
+
+//Multiple
+echo "Multiplication: " . ($x*$y). "\n";
+
+//Division
+echo "Division: " . ($x/$y). "\n";
+
+//Exponentaition
+echo "Exponentaition: " . ($x**$y). "\n";
+
+//Modulus
+echo "Modulus: " . ($x % $y) . "\n";
+if ($x == 50 and & $y == 30)
+echo "and success \n";
+
+if ($x == 50 or & $y == 20)
+echo "or success \n";
+
+if ($x == 50 xor & $y == 20)
+echo "xor success \n";
+
+if ($x == 50 && & $y == 30)
+echo "&& success \n";
+
+if ($x == 50 || & $y == 20)
+echo "|| success \n";
+
+if (!$z)
+echo "! success \n";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sample.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<h1>My first PHP Page</h1>
+
+<h2><?php echo'Hello World!';?></h2>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+

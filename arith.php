a68ae7aaef60ff939951c91abd4c1fd55d646b30
emssurya <emssurya56@gmail.com>
2025-12-29 14:41:55 +0500

    Add files via upload

---
+++ arith.php
@@ -0,0 +1,19 @@
+<?php
+// Define two numbers
+$a = 10;
+$b = 5;
+
+// Arithmetic operations
+$sum = $a + $b;
+$diff = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+$remainder = $a % $b;
+
+// Display results
+echo "Addition: $a + $b = $sum<br>";
+echo "Subtraction: $a - $b = $diff<br>";
+echo "Multiplication: $a * $b = $product<br>";
+echo "Division: $a / $b = $quotient<br>";
+echo "Modulus: $a % $b = $remainder<br>";
+?>

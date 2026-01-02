f35801461b31bd5ffd36c1c544c8bf13cf43d77c
emssurya <emssurya56@gmail.com>
2026-01-02 10:18:46 +0530

    Add files via upload

---
+++ image.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Testing";
+$heading = "Butterfly";
+$image = "images/B.jpg";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h2><?php echo $heading; ?></h2>
+    <img src="<?php echo $image; ?>" alt="My Image" width="300">
+</body>
+</html>

85a73078a0dda79dfc70dedbddc421de1c15cd13
emssurya <emssurya56@gmail.com>
2026-02-03 09:50:58 +0530

    Add Last image.php to display images
    Create a PHP file to display images from a directory.

---
+++ Last image.php
@@ -0,0 +1,39 @@
+mago
+
+Aim-
+
+Program:
+
+<html>
+
+<head>
+
+<title> IMAGE </kikle>
+
+</head>
+
+<body>
+
+<?PHP
+
+$img = glob Cimages/*. £jpg, png, JPG, PNGTY"
+
+و GLOB-BRACE(;
+
+echo "showing all imagos Edder: <br>";
+
+echo "<Pres";
+
+Point-r($img);
+
+echo "</Pore>";
+
+echo" showing the images using html forom image folder <br>
+
+forach ($ing as $image) &
+
+echo "<img src="$image' width='100'px'> <br>";
+
+</body>
+
+</html>
